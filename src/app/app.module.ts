36bfc9b76bb5dbdcca05247a1702587de8943c5b
ksmai <kasingmai@gmail.com>
2018-04-28 14:52:07 +0800

    Add service worker module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { ServiceWorkerModule } from '@angular/service-worker';
 
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
 
 
 @NgModule({
@@ -12,6 +14,9 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
+    ServiceWorkerModule.register('/ngsw-worker.js', {
+      enabled: environment.production,
+    }),
     AppRoutingModule
   ],
   providers: [],
