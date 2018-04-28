a2e45abb843f634e4e4caeba1dae6e08527d0c55
ksmai <kasingmai@gmail.com>
2018-04-28 18:12:03 +0800

    Add HomeRoutingModule

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatButtonModule } from '@angular/material';
+
 import { HomeComponent } from './home/home.component';
+import { HomeRoutingModule } from './home-routing.module';
 
 @NgModule({
   imports: [
     CommonModule,
     MatButtonModule,
+
+    HomeRoutingModule,
   ],
   declarations: [HomeComponent]
 })
