7ce5735943d98617ef33f2a9391b7a1dd5dba75f
ksmai <kasingmai@gmail.com>
2018-04-29 13:59:34 +0800

    Implement ScoreScene for concentration

---
+++ src/app/concentration/concentration-game/score.scene.ts
@@ -0,0 +1,39 @@
+import * as Phaser from 'phaser';
+
+export class ScoreScene extends Phaser.Scene {
+  active = false;
+  private moveCount: number;
+  private now: number;
+
+  constructor() {
+    super({ key: 'ScoreScene' });
+  }
+
+  init(params: { [key: string]: any }) {
+    this.moveCount = params.moveCount;
+    this.now = Date.now();
+  }
+
+  create() {
+    this.add.text(30, 32, 'Congratulations!')
+      .setStroke('#c93f3f', 3)
+      .setFontSize(24)
+      .setColor('#c93f3f');
+    this.add.text(96, 64, `You won in
+${this.moveCount}
+moves`)
+      .setStroke('#c93f3f', 3)
+      .setFontSize(16)
+      .setAlign('center')
+      .setColor('#ffffff');
+  }
+
+  update() {
+    if (Date.now() - this.now < 1500) {
+      return;
+    }
+    if (this.input.activePointer.isDown) {
+      this.scene.start('PlayScene');
+    }
+  }
+}
