b4f75a54670ca3e589bac9b9cb21bbed8e0f6adf
ksmai <kasingmai@gmail.com>
2018-05-01 20:34:09 +0800

    Remove shortcut from the path generated

--- src/app/tower-defense/tower-defense-game/path-generator.ts
+++ src/app/tower-defense/tower-defense-game/path-generator.ts
@@ -40,6 +40,7 @@ export class PathGenerator {
         [endX, endY] = this.getRandomPointAtEdge();
       } while (Math.abs(startX - endX) + Math.abs(startY - endY) < 2);
       path = tree.search(startY * this.width + startX, endY * this.width + endX);
+      this.shortcut(path);
       retryCount += 1;
     } while (path.length < minWidth);
 
@@ -58,5 +59,28 @@ export class PathGenerator {
       y = Math.floor(Math.random() * (this.height - 2)) + 1;
     }
     return [x, y];
+  }
+
+  shortcut(path: number[]): void {
+    let hasShortcut = true;
+    while (hasShortcut) {
+      hasShortcut = false;
+      for (let i = 0; i < path.length - 2; i++) {
+        const y = Math.floor(path[i] / this.width);
+        const x = path[i] % this.width;
+        for (let j = i + 2; j < path.length; j++) {
+          const y2 = Math.floor(path[j] / this.width);
+          const x2 = path[j] % this.width;
+          if (Math.abs(y - y2) + Math.abs(x - x2) <= 1) {
+            path.splice(i + 1, j - i - 1);
+            hasShortcut = true;
+            break;
+          }
+        }
+        if (hasShortcut) {
+          break;
+        }
+      }
+    }
   }
 }
